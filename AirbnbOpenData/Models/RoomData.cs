a03c322d075b0ae9860b6dcf1cc7c217deebf62d
lothar100 <infrared.skittle@gmail.com>
2025-02-15 18:26:38 -0500

    fully parsed to RoomData

--- AirbnbOpenData/Models/RoomData.cs
+++ AirbnbOpenData/Models/RoomData.cs
@@ -5,36 +5,34 @@ namespace AirbnbOpenData.Models
     [DynamoDBTable("airbnb-open-data")]
     public class RoomData
     {
-        [DynamoDBHashKey] public string id { get; set; }
-        [DynamoDBRangeKey] public string moduleType { get; set; }
-        //id,
-        //NAME,
-        //host id,
-        //host_identity_verified,
-        //host name,
-        //neighbourhood group,
-        //neighbourhood,
-        //lat,
-        //long,
-        //country,
-        //country code,
-        //instant_bookable,
-        //cancellation_policy,
-        //room type,
-        //Construction year,
-        //price,
-        //service fee,
-        //minimum nights,
-        //number of reviews,
-        //last review,
-        //reviews per month,
-        //review rate number,
-        //calculated host listings count,
-        //availability 365,
-        //house_rules,
-        //license
+        [DynamoDBHashKey] public long id { get; set; }
+        [DynamoDBRangeKey] public string dataType { get; set; }
 
-        [DynamoDBProperty] public string subType { get; set; }
-        [DynamoDBProperty] public int sortOrder { get; set; }
+        [DynamoDBProperty] public string name { get; set; }
+        [DynamoDBProperty] public long hostId { get; set; }
+        [DynamoDBProperty] public string hostIdentityVerified { get; set; }
+        [DynamoDBProperty] public string hostName { get; set; }
+        [DynamoDBProperty] public string neighbourhoodGroup { get; set; }
+        [DynamoDBProperty] public string neighbourhood { get; set; }
+        [DynamoDBProperty] public double lat { get; set; }
+        [DynamoDBProperty] public double @long { get; set; }
+        [DynamoDBProperty] public string country { get; set; }
+        [DynamoDBProperty] public string countryCode { get; set; }
+        [DynamoDBProperty] public bool? instantBookable { get; set; }
+        [DynamoDBProperty] public string cancellationPolicy { get; set; }
+        [DynamoDBProperty] public string roomType { get; set; }
+        [DynamoDBProperty] public int? constructionYear { get; set; }
+        [DynamoDBProperty] public double price { get; set; }
+        [DynamoDBProperty] public double serviceFee { get; set; }
+        [DynamoDBProperty] public int minNights { get; set; }
+        [DynamoDBProperty] public int reviewCount { get; set; }
+        [DynamoDBProperty] public DateTime? lastReviewDate { get; set; }
+        [DynamoDBProperty] public double reviewsPerMonth { get; set; }
+        [DynamoDBProperty] public int reviewRating { get; set; }
+        [DynamoDBProperty] public int hostListingCount { get; set; }
+        [DynamoDBProperty] public int daysPerYearAvailable { get; set; }
+        [DynamoDBProperty] public string roomRulesDescription { get; set; }
+        [DynamoDBProperty] public string license { get; set; }
+
     }
 }
--- AirbnbOpenData/Services/TestService.cs
+++ AirbnbOpenData/Services/TestService.cs
@@ -19,8 +19,47 @@ namespace AirbnbOpenData.Services
                 using var reader = new StreamReader(csvStream);
                 using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-                var records = csv.GetRecords<RawRoomData>().ToList();
-                var top10 = records.Take(10).ToList();
+                var records = csv.GetRecords<RawRoomData>().Select(x =>
+                    new RoomData()
+                    {
+                        id = x.id,
+                        dataType = "Room",
+                        name = x.name,
+                        hostId = x.hostId,
+                        hostIdentityVerified = x.hostIdentityVerified,
+                        hostName = x.hostName,
+                        neighbourhoodGroup = x.neighbourhoodGroup,
+                        neighbourhood = x.neighbourhood,
+                        lat = double.TryParse(x.lat, out double latResult) ? latResult : 0,
+                        @long = double.TryParse(x.@long, out double longResult) ? longResult : 0,
+                        country = x.country,
+                        countryCode = x.countryCode,
+                        instantBookable = bool.TryParse(x.instantBookable, out bool bookableResult) ? bookableResult : null,
+                        cancellationPolicy = x.cancellationPolicy,
+                        roomType = x.roomType,
+                        constructionYear = int.TryParse(x.constructionYear, out int yearResult) ? yearResult : null,
+                        price = double.TryParse(x.price, out double priceResult) ? priceResult : 0,
+                        serviceFee = double.TryParse(x.serviceFee, out double serviceFeeResult) ? serviceFeeResult : 0,
+                        minNights = int.TryParse(x.minNights, out int minNightResult) ? minNightResult : 0,
+                        reviewCount = int.TryParse(x.reviewCount, out int reviewCountResult) ? reviewCountResult : 0,
+                        lastReviewDate = DateTime.TryParse(x.lastReviewDate, out DateTime dateResult) ? dateResult : null,
+                        reviewsPerMonth = double.TryParse(x.reviewsPerMonth, out double reviewsPerMonthResult) ? reviewsPerMonthResult : 0,
+                        reviewRating = int.TryParse(x.reviewRating, out int reviewRatingResult) ? reviewRatingResult : 0,
+                        hostListingCount = int.TryParse(x.hostListingCount, out int hostListingCountResult) ? hostListingCountResult : 0,
+                        daysPerYearAvailable = int.TryParse(x.daysPerYearAvailable, out int daysPerYearAvailableResult) ? daysPerYearAvailableResult : 0,
+                        roomRulesDescription = x.roomRulesDescription,
+                        license = x.license
+                    }
+                )
+                 .ToList();
+                var top10Parsed = records.Take(10).ToList();
+
+                // INSERT CODE HERE TO UPLOAD TO AWS
+                // RoomData already knows to point to which dynamoDb by name: [DynamoDBTable("airbnb-open-data")]
+                // I might have to create the table first with Id and dataType
+                // You also need local credentials which are activated with an AWS command line
+                // I might need to add Drew to my AWS account so he can get permissions
+                // Program.cs has commented code for additional appsettings, but it's just Region setup as 'us-east-2' (but a good example of how to not publicly release your settings)
 
                 int breakpoint = 1;
             }
